51b4e745376c0a6165dc58922a48581706abb3be
alfredwang0707 <alfredwang0707@gmail.com>
2021-05-16 22:50:44 -0400

    add event emitter/request/bigfile page

---
+++ 13-event-emitter.js
@@ -0,0 +1,17 @@
+const EventEmitter = require('events')
+
+const customEmitter = new EventEmitter()
+
+// on - listen for event
+customEmitter.on('response',(name,id)=>{
+    console.log(`data receieved user ${name} with id:${id}`);
+})
+
+customEmitter.emit('response')
+customEmitter.on('response',()=>{
+    console.log('some other logic here'); 
+})
+
+customEmitter.emit('response', 'alfred,', 34)
+
+
